004d0e3191a472558d525b17b5aa5372df0ada0c
Rob Lanphier <robla@wikimedia.org>
2011-09-11 18:03:51 -0700

    Simplified and commented

--- resources/mw.jsondata.js
+++ resources/mw.jsondata.js
@@ -2,15 +2,13 @@
 // Copyright (c) 2011 Rob Lanphier.
 // See http://robla.net/jsonwidget/LICENSE for license (3-clause BSD-style)
 
-function sample_init() {
+// check for the jsonedit form div
+if($("#je_formdiv").length > 0) {
+    //initialize jsonwidget editor (jsonedit.js)
     jsonwidget.language = 'en';
     var je=new jsonwidget.editor();
 	je.htmlids.sourcetextarea = 'wpTextbox1';
 	je.htmlids.sourcetextform = 'editform';
     je.setView('form');
-}
-
-if($("#je_formdiv").length > 0) {
-	sample_init();
 }
 
