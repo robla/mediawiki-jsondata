dc84c719af2569fa0ecb66b2c81a7820956f3fa4
Rob Lanphier <robla@wikimedia.org>
2011-10-08 21:01:11 -0700

    Renamed LocalSettings_example, and added another namespace

--- LocalSettings_example.php
+++ LocalSettings_example.php
@@ -1,10 +1,15 @@
+<?php
 # Example PHP
 require_once("$IP/extensions/JsonData/JsonData.php");
+
 $wgJsonDataNamespace[202] = "Data";
 //$wgJsonDataSchemaFile[202] = "$IP/extensions/JsonData/schemas/simpleaddr-schema.json";
 $wgJsonDataSchemaArticle[202] = "Schema:SimpleAddr";
 $wgJsonDataNamespace[204] = "Schema";
 $wgJsonDataSchemaFile[204] = "$IP/extensions/JsonData/schemas/schemaschema.json";
+
+$wgJsonDataNamespace[206] = "JsonConfig";
+$wgJsonDataSchemaArticle[206] = "Schema:JsonConfig";
 
 foreach ($wgJsonDataNamespace as $nsnum => $nskey) {
 	$wgExtraNamespaces[$nsnum] = $nskey;
