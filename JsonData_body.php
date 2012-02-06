robla/mediawiki-jsondata

commit a5fa9bd4732b4f3d148a03f2266eb4ce9fb9352e

Made it so that the schema attribute can be changed on preview

JsonData_body.php
@@ -107,11 +107,10 @@ HEREDOC
 		$config = $this->getConfig();
 		$tag = $config['namespaces'][$this->nsname]['defaulttag'];
 
-
 		$schemaconfig = $config['tags'][$tag]['schema'];
 		$schemaTitle = null;
 		if ( isset( $schemaconfig['schemaattr'] ) && ( preg_match( '/^(\w+)$/', $schemaconfig['schemaattr'] ) > 0 ) ) {
-			$revtext = $this->article->getText();
+			$revtext = $this->out->getRequest()->getText( 'wpTextbox1' );
 			if ( preg_match( '/^<[\w]+\s+([^>]+)>/m', $revtext, $matches ) > 0 ) {
 				/*
 				 * Quick and dirty regex for parsing schema attributes that hits the 99% case.

commit 280b42b99d4c5b48732b89b90860ed8fbf684fb2

Fixed schema attribute for normal editing (broken in a5fa9bd4732b4f)

JsonData_body.php
@@ -127,7 +127,13 @@ HEREDOC
 		$schemaconfig = $config['tags'][$tag]['schema'];
 		$schemaTitle = null;
 		if ( isset( $schemaconfig['schemaattr'] ) && ( preg_match( '/^(\w+)$/', $schemaconfig['schemaattr'] ) > 0 ) ) {
+			// on preview, pull $revtext out from the submitted text, so that the author can change
+			// schemas during preview
 			$revtext = $this->out->getRequest()->getText( 'wpTextbox1' );
+			// wpTextbox1 is empty in normal editing, so pull it from article->getText() instead
+			if ( empty( $revtext ) ) {
+				$revtext = $this->article->getText();
+			}
 			if ( preg_match( '/^<[\w]+\s+([^>]+)>/m', $revtext, $matches ) > 0 ) {
 				/*
 				 * Quick and dirty regex for parsing schema attributes that hits the 99% case.